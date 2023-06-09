3dc0f21b61348c7bdb2720c6484a1120627563f0
Katalina Okano <git@kat.cafe>
2023-06-09 08:58:54 -0400

    swap to sqlite, sled might be too slow

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use axum::{
     routing::get,
     Json, Router,
 };
+use r2d2_sqlite::{rusqlite::params, SqliteConnectionManager};
 use std::{env, net::SocketAddr};
 use tokio::task::JoinSet;
 use types::{World, Zone};
@@ -24,20 +25,32 @@ async fn main() {
         // .with_max_level(Level::DEBUG)
         .init();
 
-    let db = sled::open("/tmp/metagame").expect("open");
+    // let db = sqlite::Connection::open_with_full_mutex(":memory:").unwrap();
+    // db.execute("CREATE TABLE worlds (id INTEGER NOT NULL PRIMARY KEY, data BLOB);")
+    //     .unwrap();
+
+    let sqlite_manager = SqliteConnectionManager::memory();
+    let pool = r2d2::Pool::new(sqlite_manager).unwrap();
+    pool.get()
+        .unwrap()
+        .execute(
+            "CREATE TABLE worlds (id INTEGER NOT NULL PRIMARY KEY, data BLOB);",
+            params![],
+        )
+        .unwrap();
 
     let app = Router::new()
         .layer(tower_http::trace::TraceLayer::new_for_http())
         .route("/:world", get(get_one_world))
         .route("/all", get(get_all_worlds))
         .route("/", get(root))
-        .with_state(db.clone());
+        .with_state(pool.clone());
 
     tokio::spawn(async move {
         loop {
             let mut set = JoinSet::new();
             for world in vec![1, 10, 13, 17, 19, 40, 1000, 2000] {
-                set.spawn(get_world(db.clone(), world, true));
+                set.spawn(get_world(pool.clone(), world, true));
             }
 
             while let Some(_) = set.join_next().await {}
@@ -64,11 +77,16 @@ async fn root() -> Html<&'static str> {
     Html(include_str!("./html/index.html"))
 }
 
-pub async fn get_one_world(State(db): State<sled::Db>, Path(world): Path<i32>) -> Json<World> {
+pub async fn get_one_world(
+    State(db): State<r2d2::Pool<SqliteConnectionManager>>,
+    Path(world): Path<i32>,
+) -> Json<World> {
     Json(get_world(db, world, false).await)
 }
 
-pub async fn get_all_worlds(State(db): State<sled::Db>) -> Json<Vec<World>> {
+pub async fn get_all_worlds(
+    State(db): State<r2d2::Pool<SqliteConnectionManager>>,
+) -> Json<Vec<World>> {
     let mut set = JoinSet::new();
     let mut worlds = vec![World::default(); 8];
 
@@ -85,7 +103,11 @@ pub async fn get_all_worlds(State(db): State<sled::Db>) -> Json<Vec<World>> {
     Json(worlds)
 }
 
-pub async fn get_world(db: sled::Db, world: i32, skip_cache: bool) -> World {
+pub async fn get_world(
+    db: r2d2::Pool<SqliteConnectionManager>,
+    world: i32,
+    skip_cache: bool,
+) -> World {
     if !skip_cache {
         match world_from_cache(db.clone(), world) {
             Ok(response) => return response,
@@ -114,19 +136,21 @@ pub async fn get_world(db: sled::Db, world: i32, skip_cache: bool) -> World {
         cached_at: chrono::Utc::now(),
     };
 
-    world_to_cache(db, world, &response);
+    world_to_cache(db.clone(), world, &response);
 
     response
 }
 
-fn world_from_cache(db: sled::Db, world: i32) -> Result<World, ()> {
-    let key = format!("world:{}", world);
-    let value = match db.get(key) {
-        Ok(Some(value)) => value,
-        _ => return Err(()),
-    };
+fn world_from_cache(db: r2d2::Pool<SqliteConnectionManager>, world: i32) -> Result<World, ()> {
+    let db = db.get().unwrap();
+    let mut query = db.prepare("SELECT data FROM worlds WHERE id = ?").unwrap();
+    let value: Result<Vec<u8>, _> = query.query_row(params![world], |r| r.get(0));
 
-    match bincode::deserialize::<World>(&value) {
+    if value.is_err() {
+        return Err(());
+    }
+
+    match bincode::deserialize::<World>(value.unwrap().as_slice()) {
         Ok(response) => {
             if response.cached_at + chrono::Duration::minutes(5) < chrono::Utc::now() {
                 return Err(());
@@ -137,8 +161,9 @@ fn world_from_cache(db: sled::Db, world: i32) -> Result<World, ()> {
     }
 }
 
-fn world_to_cache(db: sled::Db, world: i32, response: &World) {
-    let key = format!("world:{}", world);
+fn world_to_cache(db: r2d2::Pool<SqliteConnectionManager>, world: i32, response: &World) {
     let value = bincode::serialize(response).unwrap();
-    db.insert(key, value).unwrap();
+    let db = db.get().unwrap();
+    let mut query = db.prepare("INSERT INTO worlds (id, data) VALUES (?, ?) ON CONFLICT DO UPDATE SET data=excluded.data").unwrap();
+    query.execute(params![world, value]).unwrap();
 }
