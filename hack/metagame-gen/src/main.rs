3dfa4f63f388451e93f8726d637b5ad831527879
Katalina Okano <git2@kat.cafe>
2023-07-16 14:22:06 -0400

    differentiate unstable meltdown

--- hack/metagame-gen/src/main.rs
+++ hack/metagame-gen/src/main.rs
@@ -11,6 +11,12 @@ struct MetagameEvent {
     metagame_event_id: String,
     #[serde(rename = "type", deserialize_with = "deserialize_number_from_string")]
     event_type: i32,
+    name: LangEn,
+}
+
+#[derive(Deserialize)]
+struct LangEn {
+    en: String,
 }
 
 #[tokio::main]
@@ -29,13 +35,15 @@ pub fn alert_type(metagame_event_id: i32) -> String {{
     match metagame_event_id {{
         {} => \"air\".to_string(),
         {} => \"sudden_death\".to_string(),
+        {} => \"unstable_meltdown\".to_string(),
         {} | _ => \"conquest\".to_string(),
     }}
 }}",
-    metagame_events.metagame_event_list.iter().filter(|e| e.event_type == 10).map(|e| e.metagame_event_id.clone()).collect::<Vec<String>>().join(" | "),
-    metagame_events.metagame_event_list.iter().filter(|e| e.event_type == 6).map(|e| e.metagame_event_id.clone()).collect::<Vec<String>>().join(" | "),
-    metagame_events.metagame_event_list.iter().filter(|e| e.event_type == 9).map(|e| e.metagame_event_id.clone()).collect::<Vec<String>>().join(" | "),
-);
+        metagame_events.metagame_event_list.iter().filter(|e| e.event_type == 10).map(|e| e.metagame_event_id.clone()).collect::<Vec<String>>().join(" | "),
+        metagame_events.metagame_event_list.iter().filter(|e| e.event_type == 6).map(|e| e.metagame_event_id.clone()).collect::<Vec<String>>().join(" | "),
+        metagame_events.metagame_event_list.iter().filter(|e| e.event_type == 9 && e.name.en.contains("eltdown")).map(|e| e.metagame_event_id.clone()).collect::<Vec<String>>().join(" | "),
+        metagame_events.metagame_event_list.iter().filter(|e| e.event_type == 9 && !e.name.en.contains("eltdown")).map(|e| e.metagame_event_id.clone()).collect::<Vec<String>>().join(" | "),
+    );
 
     std::fs::write("../../src/alert_types.rs", template).unwrap();
 
